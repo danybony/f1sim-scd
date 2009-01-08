598cac6f7935bd30b489170423fc556bffc1499b
dany.bony <dany.bony@e07abdb6-dcc9-11dd-a893-8d7ce9fbef62>
2009-01-08 14:00:13 +0000

    Creato il progetto che va a prendersi i files nella cartella "src". Poi fatta la definizione di pilota. Preso spunto dal progetto dei treni!

---
+++ trunk/src/circuit/circuit-types.ads
@@ -0,0 +1,29 @@
+--------------------------------------------------------
+--  Copyright (C) 2009                                --
+--  Daniele Bonaldo, Alberto Zatton                   --
+--                                                    --
+--  F1Sim-scd 1.0                                     --
+--                                                    --
+--  Concurrent and Distributed Systems class project  --
+--  Master Degree in Computer Science                 --
+--  Academic year 2008/2009                           --
+--  Dept. of Pure and Applied Mathematics             --
+--  University of Padua, Italy                        --
+--                                                    --
+--  This program is free software; you can            --
+--  redistribute it and/or modify it under the terms  --
+--  of the GNU General Public License as published    --
+--  by the Free Software Foundation; either           --
+--  version 2 of the License, or (at your option)     --
+--  any later version.                                --
+--------------------------------------------------------
+
+--  Package with util type definitions
+
+package Circuit.Types is
+
+   type String_Ref_T       is access all String;
+   type Integer_Ref_T      is access all Integer;
+
+end Circuit.Types;
+
