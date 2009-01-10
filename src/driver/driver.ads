5b2cf9fff8cddae9c7fdf9369635f9e6d9d0bf1b
alzatxxx <alzatxxx@gmail.com>
2009-01-10 13:42:27 +0000

    Organizzati i file del pilota: -creata la cartella src/driver -spostato "pilot.ads" in src/driver -rinominato "pilot.ads" in "driver.ads" -estrapolati i types usati in "driver.ads" e inseriti in "driver_types.ads" -corretti i types usati in "driver.ads"

---
+++ src/driver/driver.ads
@@ -0,0 +1,49 @@
+--------------------------------------------------------
+--  Copyright (C) 2009                                --
+--  Daniele Bonaldo, Alberto Zatton                   --
+--                                                    --
+--  F1Sim-scd 1.0                                     --
+--                                                    --
+--  Concurrent and Distributed Systems class project  --
+--  Master Degree in Computer Science                 --
+--  Academic year 2008/2009                           --
+--  Dept. of Pure and Applied Mathematics             --
+--  University of Padua, Italy                        --
+--                                                    --
+--  This program is free software; you can            --
+--  redistribute it and/or modify it under the terms  --
+--  of the GNU General Public License as published    --
+--  by the Free Software Foundation; either           --
+--  version 2 of the License, or (at your option)     --
+--  any later version.                                --
+--------------------------------------------------------
+
+--with Race.Types; use Race.Types;
+--with Race; use Race;
+with Race.Driver.Types; use Race.Driver.Types;
+
+--  Questo package contiene la definizione dell'interfaccia del pilota (driver),
+--+ rappresentato dal task type "Driver". La definizione del body è contenuta
+--+ nel file "driver.adb" presente in src/driver.
+package Race.Driver is
+
+   task type Driver
+     (Name		: String;
+      Car		: Scuderia_ID_T
+      Number		: Positive;
+      Accel_Coeff	: Accel_Coeff_T;
+      Break_Coeff	: Break_Coeff_T;
+      Max_Speed		: Real;
+      Strategy		: Strategy_T
+      Position		: Positive;
+      Laps_Done		: Positive;
+      Prior		: Positive;
+     ) is
+
+      pragma Priority (Prior);
+
+   end Driver;
+
+   type Driver_Ref is access all Driver;
+
+end Race.Driver;
