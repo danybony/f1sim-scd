51abf4edd3c42c3c121b7206373524c12619eab6
alzatxxx <alzatxxx@e07abdb6-dcc9-11dd-a893-8d7ce9fbef62>
2009-01-10 13:42:27 +0000

    Organizzati i file del pilota: -creata la cartella src/driver -spostato "pilot.ads" in src/driver -rinominato "pilot.ads" in "driver.ads" -estrapolati i types usati in "driver.ads" e inseriti in "driver_types.ads" -corretti i types usati in "driver.ads"

---
+++ trunk/src/driver/driver_types.ads
@@ -0,0 +1,25 @@
+--------------------------------------------------------
+--  Copyright (C) 2009                                --
+--  Daniele Bonaldo, Alberto Zatton                   --
+--                                                    --
+--  F1Sim-scd 1.0                                     --
+--                                                    --
+--  Concurrent and Distributed Systems class project  --
+--  Master Degree in Computer Science                 --
+--  Academic year 2008/2009                           --
+--  Dept. of Pure and Applied Mathematics             --
+--  University of Padua, Italy                        --
+--                                                    --
+--  This program is free software; you can            --
+--  redistribute it and/or modify it under the terms  --
+--  of the GNU General Public License as published    --
+--  by the Free Software Foundation; either           --
+--  version 2 of the License, or (at your option)     --
+--  any later version.                                --
+--------------------------------------------------------
+
+package Race.Driver.Types is
+
+	type Positive is subtype Integer range 1..Integer'last;
+
+end Race.Driver.Types
