c04f6a7e3387d9f2b980d7205ffdcd3a232d9247
alzatxxx <alzatxxx@e07abdb6-dcc9-11dd-a893-8d7ce9fbef62>
2009-01-23 19:11:18 +0000

    Sperando che sia la volta buona, cambiati package e sottopackage secondo gli standard di Tullio; Commentato "Segment_T" in quanto genera un errore a compile time

--- trunk/src/race/race-startup.ads
+++ trunk/src/race/race-startup.ads
@@ -18,19 +18,8 @@
 --  any later version.                                --
 --------------------------------------------------------
 
-package Race is
-
-   subtype Positive 	is Integer range 1..Integer'last;
-   type String_Ref_T       is access all String;
-   type Integer_Ref_T      is access all Integer;
-
-   --type Macro_Segment_T;
-   --type Macro_Segment_Ref_T is access all Macro_Segment_T;
-   subtype Substring_T is String(1..255);
-   type Substring_array_T is array (Positive range <>) of Substring_T;
-   type Strategy_T is array (Positive range <>) of Positive;
 
 
-
-end Race;
-
+package Race.Startup is
+   procedure startup;
+end Race.Startup;
